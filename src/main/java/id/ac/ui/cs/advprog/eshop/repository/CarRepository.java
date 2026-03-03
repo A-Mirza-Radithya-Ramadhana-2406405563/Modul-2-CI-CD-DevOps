f06db04f3749a7529ffca679e7a05211325fbbfe
JaaTzy14 <mirzaradithya1410@gmail.com>
2026-03-03 12:20:34 +0700

    refactor: improve codes readability

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/CarRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/CarRepository.java
@@ -1,5 +1,6 @@
 package id.ac.ui.cs.advprog.eshop.repository;
 
+import id.ac.ui.cs.advprog.eshop.exception.CarNotFoundException;
 import id.ac.ui.cs.advprog.eshop.model.Car;
 import org.springframework.stereotype.Repository;
 import java.util.ArrayList;
@@ -31,29 +32,21 @@ public class CarRepository {
     }
 
     public Car findById(String id) {
-        for (Car car : carData) {
-            if (car.getCarId().equals(id)) {
-                return car;
-            }
-        }
-        return null;
+        return carData.stream()
+                .filter(car -> car.getCarId().equals(id))
+                .findFirst()
+                .orElseThrow(() -> new CarNotFoundException(id));
     }
 
     public Car update(String id, Car updatedCar) {
-        for (int i = 0; i < carData.size(); i++) {
-            Car car = carData.get(i);
-            if (car.getCarId().equals(id)) {
-                car.setCarName(updatedCar.getCarName());
-                car.setCarColor(updatedCar.getCarColor());
-                car.setCarQuantity(updatedCar.getCarQuantity());
-                return car;
-            }
-        }
-        return null;
+        Car car = findById(id);
+        car.setCarName(updatedCar.getCarName());
+        car.setCarColor(updatedCar.getCarColor());
+        car.setCarQuantity(updatedCar.getCarQuantity());
+        return car;
     }
 
     public void delete(String id) {
         carData.removeIf(car -> car.getCarId().equals(id));
     }
-
 }
