c1d006c96cad1807442095611010495ec1391731
JaaTzy14 <mirzaradithya1410@gmail.com>
2026-03-03 12:44:03 +0700

    fix: remove unused imports

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/CarRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/CarRepository.java
@@ -1,12 +1,8 @@
 package id.ac.ui.cs.advprog.eshop.repository;
 
-import id.ac.ui.cs.advprog.eshop.exception.CarNotFoundException;
 import id.ac.ui.cs.advprog.eshop.model.Car;
 import org.springframework.stereotype.Repository;
-import java.util.ArrayList;
 import java.util.Iterator;
-import java.util.List;
-import java.util.UUID;
 
 @Repository
 public interface CarRepository {
--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepository.java
@@ -1,12 +1,9 @@
 package id.ac.ui.cs.advprog.eshop.repository;
 
-import id.ac.ui.cs.advprog.eshop.exception.ProductNotFoundException;
 import id.ac.ui.cs.advprog.eshop.model.Product;
 import org.springframework.stereotype.Repository;
 
-import java.util.ArrayList;
 import java.util.Iterator;
-import java.util.List;
 
 @Repository
 public interface ProductRepository {
