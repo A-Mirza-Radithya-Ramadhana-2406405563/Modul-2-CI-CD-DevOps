4779610abcd60eff760ee07f5f0dcb2cf72333e3
JaaTzy14 <mirzaradithya1410@gmail.com>
2026-03-03 10:30:06 +0700

    test: add unit test for Car repository

---
+++ src/test/java/id/ac/ui/cs/advprog/eshop/repository/CarRepositoryTest.java
@@ -0,0 +1,129 @@
+package id.ac.ui.cs.advprog.eshop.repository;
+
+import id.ac.ui.cs.advprog.eshop.model.Car;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Iterator;
+
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(MockitoExtension.class)
+public class CarRepositoryTest {
+    @InjectMocks
+    CarRepository carRepository;
+
+    @Test
+    void testCreateAndFind() {
+        Car car = new Car();
+        car.setCarId("eb558e9f-1c39-460e-8860-71af6af63bd6");
+        car.setCarName("Sedan Cap Bambang");
+        car.setCarColor("ungu");
+        car.setCarQuantity(1);
+        carRepository.createCar(car);
+
+        Iterator<Car> carIterator = carRepository.findAll();
+        assertTrue(carIterator.hasNext());
+        Car savedcar = carIterator.next();
+        assertEquals(car.getCarId(), savedcar.getCarId());
+        assertEquals(car.getCarName(), savedcar.getCarName());
+        assertEquals(car.getCarColor(), savedcar.getCarColor());
+        assertEquals(car.getCarQuantity(), savedcar.getCarQuantity());
+    }
+
+    @Test
+    void testFindAllIfEmpty() {
+        Iterator<Car> carIterator = carRepository.findAll();
+        assertFalse(carIterator.hasNext());
+    }
+
+    @Test
+    void testFindAllIfMoreThanOneCar() {
+        Car car1 = new Car();
+        car1.setCarId("eb558e9f-1c39-460e-8860-71af6af63bd6");
+        car1.setCarName("Sedan Cap Bambang");
+        car1.setCarColor("ungu");
+        car1.setCarQuantity(1);
+        carRepository.createCar(car1);
+
+        Car car2 = new Car();
+        car2.setCarId("a0f9de46-90b1-437d-a0bf-d0821dde9906");
+        car2.setCarName("Sedan Cap Usep");
+        car2.setCarColor("merah");
+        car2.setCarQuantity(5);
+        carRepository.createCar(car2);
+
+        Iterator<Car> carIterator = carRepository.findAll();
+        assertTrue(carIterator.hasNext());
+        Car savedCar = carIterator.next();
+        assertEquals(car1.getCarId(), savedCar.getCarId());
+        savedCar = carIterator.next();
+        assertEquals(car2.getCarId(), savedCar.getCarId());
+        assertFalse(carIterator.hasNext());
+    }
+
+    @Test
+    void testFindByIdIfCarExists() {
+        Car car = new Car();
+        car.setCarId("eb558e9f-1c39-460e-8860-71af6af63bd6");
+        car.setCarName("Sedan Cap Bambang");
+        car.setCarColor("ungu");
+        car.setCarQuantity(100);
+        carRepository.createCar(car);
+
+        Car savedCar = carRepository.findById(car.getCarId());
+        assertEquals(car, savedCar);
+    }
+
+    @Test
+    void testFindByIdIfCarDoesNotExist() {
+        assertNull(carRepository.findById("NO_ID"));
+    }
+
+    @Test
+    void testUpdateCarIfCarExists() {
+        Car car = new Car();
+        car.setCarId("eb558e9f-1c39-460e-8860-71af6af63bd6");
+        car.setCarName("Sedan Cap Bambang");
+        car.setCarColor("ungu");
+        car.setCarQuantity(100);
+        carRepository.createCar(car);
+
+        Car editedCar = new Car();
+        editedCar.setCarName("Sedan Cap");
+        editedCar.setCarColor("merah");
+        editedCar.setCarQuantity(50);
+        carRepository.update(car.getCarId(), editedCar);
+
+        Car savedCar = carRepository.findById(car.getCarId());
+        assertEquals("Sedan Cap", savedCar.getCarName());
+        assertEquals(50, savedCar.getCarQuantity());
+        assertEquals("merah", savedCar.getCarColor());
+    }
+
+    @Test
+    void testEditCarIfCarDoesNotExist() {
+        Car editedCar = new Car();
+        editedCar.setCarName("Sedan Cap");
+        editedCar.setCarQuantity(50);
+        assertNull(carRepository.update("NO_ID", editedCar));
+    }
+
+    @Test
+    void testDeleteCarIfCarExists() {
+        Car car = new Car();
+        car.setCarId("eb558e9f-1c39-460e-8860-71af6af63bd6");
+        car.setCarName("Sampo Cap Bambang");
+        car.setCarQuantity(100);
+        carRepository.createCar(car);
+        assertEquals(car, carRepository.findById(car.getCarId()));
+        carRepository.delete(car.getCarId());
+        String id = car.getCarId();
+        assertNull(carRepository.findById(id));
+    }
+}
